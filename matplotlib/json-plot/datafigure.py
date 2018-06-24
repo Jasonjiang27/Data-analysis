a28cc6e4522817cbc16f4743b14bd4d9a1735b5f
Jasonjiang27 <jiangyz1991@163.com>
2018-06-24 10:57:22 +0800

    add plot 线性图

---
+++ matplotlib/json-plot/datafigure.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def data_plot():
+    try:
+        df = pd.read_json('user_study.json')
+    except ValueError:
+        pass
+    data = df.groupby('user_id').sum()
+    user_id = df.user_id
+    minutes = data.sum()
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)   #画布分割为1行1列的第1个
+    ax.set_title("StudyData")
+    ax.set_xlabel("User ID")
+    ax.set_ylabel("Study Time")
+    x=data.index
+    y=data.minutes
+    ax.plot(x,y)
+    plt.show()
+    return ax 
+
+if __name__=='__main__':
+    data_plot()
